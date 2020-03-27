8b5ae1bbecb8a740717132b3da47a94e022e596f
Skorpion-tracer <39811825+Skorpion-tracer@users.noreply.github.com>
2020-03-27 20:49:36 +0300

    Add files via upload

---
+++ src/sample/Main.java
@@ -0,0 +1,28 @@
+/**
+ * @author Vitaliy Brazhnik
+ * @link https://github.com/Skorpion-tracer
+ */
+
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/sample.fxml"));
+        primaryStage.setTitle("Calculator FX");
+        primaryStage.setScene(new Scene(root, 238, 300));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
